206f399e18650f1cc570c3f77215837730c0d5c5
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2020-08-24 15:08:53 -0700

    Implemented efficient recursive Fibbonaci algorithm

--- LinearRecursiveFib/LinearRecursiveFib/Program.cs
+++ LinearRecursiveFib/LinearRecursiveFib/Program.cs
@@ -6,7 +6,35 @@ namespace LinearRecursiveFib
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("input an integer n to calculate the nth fibbonanci number");
+            int n = Int32.Parse(Console.ReadLine());
+            new FibCalc(n);
+            Console.ReadLine();
         }
     }
+
+    class FibCalc
+    {
+        private long[] fibbonacis;
+
+        public FibCalc(int n)
+        {
+            fibbonacis = new long[n];
+            Console.WriteLine(CalculateNthFib(n));
+        }
+
+        private long CalculateNthFib(int n)
+        {
+            if(n<3)
+            {
+                return 1;
+            }
+            if(!(fibbonacis[n-1]>0))
+            {
+                fibbonacis[n - 1] = CalculateNthFib(n - 1) + CalculateNthFib(n - 2);
+            }
+            return fibbonacis[n - 1];
+        }
+    }
+
 }
